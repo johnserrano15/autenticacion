d7518b930347b038c60bb621be800cc96f941280
John Serrano <andrey_201015@hotmail.com>
2019-07-19 11:57:56 -0500

    Creando los clientes de Spotify y servicios iniciales

---
+++ blog/utils/scopesArray.js
@@ -0,0 +1,9 @@
+const scopesArray = [
+  "user-read-private",
+  "user-read-email",
+  "playlist-read-collaborative"
+]
+
+// https://developer.spotify.com/documentation/general/guides/scopes/
+
+module.exports = scopesArray
